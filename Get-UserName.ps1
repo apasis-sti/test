1f18254d253c2b93f06ee91f690f3328b51c5497
apasis-sti <apasis@sorrentotherapeutics.com>
2023-10-06 13:46:34 -0700

    Add Comment In Line 1

--- Get-UserName.ps1
+++ Get-UserName.ps1
@@ -1,8 +1,7 @@
-﻿
+﻿#AD Get username
         $firstname = Read-Host "Enter users first name"
         $lastname  = Read-Host "Enter Users' last name"
         $Name      = $firstname + " " + $lastname
         $username = Get-ADUser -Filter "Displayname -eq '$Name'"| Select-Object -ExpandProperty 'SamAccountName'
         write-host $username
         pause
-      
