db894edc55bc729ee9588bc60b0c2b52937384fa
Admin APasis <Admin.APasis@sorrentotherapeutics.com>
2023-10-13 12:14:31 -0700

    Add Restart-UpdatedTestServers.ps

---
+++ Restart-UpdatedTestServers.ps1
@@ -0,0 +1,13 @@
+﻿
+$csvPath = 'C:\IT\reboot_servers.csv'
+$csvData = Get-Content -Path $csvPath
+
+  
+#Store the data from ADUsers.csv in the $ADUsers variable
+$computers = $csvData
+
+
+Foreach ($computer in $computers)
+    {
+        Restart-Computer -ComputerName $computer -Force
+    }
